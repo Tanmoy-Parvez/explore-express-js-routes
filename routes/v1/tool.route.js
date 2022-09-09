9b518805649ea7d19d59631cd1eb0cf418401907
Tanmoy-Parvez <tanmoyparvez23@gmail.com>
2022-09-09 22:34:23 +0600

    external middleware added

--- routes/v1/tool.route.js
+++ routes/v1/tool.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const toolsController = require('../../controllers/v1/tools.controllers');
 const { viewCount } = require('../../middleware/viewCount');
+const reqLimiter = require('../../middleware/reqLimiter');
 const router = express.Router();
 
 /* router.get("/", (req, res) => { 
@@ -20,7 +21,7 @@ router.route("/")
 
     .post(toolsController.saveATool)
 
-router.route("/:id").get(viewCount, toolsController.getAllToolsById)
+router.route("/:id").get(viewCount, reqLimiter, toolsController.getAllToolsById)
 
 
 module.exports = router;
